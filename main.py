bd5ee6fd58e59d75c91340ce50b387902331bb26
Salman Farid Rahman <91804876+Salman-Farid@users.noreply.github.com>
2025-02-03 03:36:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from typing import Dict, List, Any
 import os
 
 from fastapi import FastAPI, File, UploadFile
-from sympy import Dict
 from torchvision import transforms
 from PIL import Image
 import torch
@@ -25,13 +23,11 @@ class_labels = [
     'Cucumber_Gummy Stem Blight', 'Eggplant - Epilachna Beetle', 'Eggplant - Flea Beetle', 'Eggplant - Healthy',
     'Eggplant - Jassid', 'Eggplant - Mite', 'Eggplant - Mite and Epilachna Beetle', 'Eggplant - Nitrogen Deficiency',
     'Eggplant - Nitrogen and Potassium Deficiency', 'Eggplant - Potassium Deficiency', 'Lentil_Ascochyta blight',
-    'Lentil_Normal', 'Lentil_Powdery Mildew', 'Lentil_Rust', 'Paddy_bacterial_leaf_blight',
-    'Paddy_bacterial_leaf_streak',
+    'Lentil_Normal', 'Lentil_Powdery Mildew', 'Lentil_Rust', 'Paddy_bacterial_leaf_blight', 'Paddy_bacterial_leaf_streak',
     'Paddy_bacterial_panicle_blight', 'Paddy_blast', 'Paddy_brown_spot', 'Paddy_dead_heart', 'Paddy_downy_mildew',
     'Paddy_hispa', 'Paddy_normal', 'Paddy_tungro', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
     'Sugarcane_Healthy', 'Sugarcane_Mosaic', 'Sugarcane_RedRot', 'Sugarcane_Rust', 'Sugarcane_Yellow',
-    'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold',
-    'Tomato_Septoria_leaf_spot',
+    'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
     'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus',
     'Tomato__Tomato_mosaic_virus', 'Tomato_healthy', 'Wheat_Brown rust', 'Wheat_Healthy', 'Wheat_Loose Smut',
     'Wheat_Mildew', 'Wheat_Septoria', 'Wheat_Stem Rust', 'Wheat_Yellow rust'
@@ -45,14 +41,9 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-
 @app.get("/")
-async def root() -> dict[str, int | str | list[str | Any]]:
-    """Root endpoint returning API information."""
-    return {
-        "total_classes": len(class_labels),
-    }
-
+async def root() -> dict[str, int]:
+    return {"total_classes": len(class_labels)}
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
@@ -71,8 +62,6 @@ async def predict(file: UploadFile = File(...)):
 
     return {"prediction": prediction}
 
-
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
